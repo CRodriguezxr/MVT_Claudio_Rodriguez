606663335e0d18bd6307a7a3423244827a29fddc
Claudio Rodriguez <claudio.rodriguez1992@gmail.com>
2022-09-09 19:21:41 -0300

    creacion tercera moto

--- MVT_Claudio_Rodriguez/Moto/views.py
+++ MVT_Claudio_Rodriguez/Moto/views.py
@@ -3,7 +3,7 @@ from Moto.models import Moto
 from datetime import datetime
 
 def nueva_moto(request):
-    nueva_moto = Moto.objects.create(marca = "Kawasaki", modelo = "Ninja", cilindrada = 400, precio = 3081500, fecha_de_creacion = datetime.today())
+    nueva_moto = Moto.objects.create(marca = "Yamaha", modelo = "MT03", cilindrada = 320, precio = 2896500, fecha_de_creacion = datetime.today())
     context = {
         'nueva_moto': nueva_moto
     }
